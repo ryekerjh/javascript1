466786cb211c78a119f1fa60b5d8206fb8641c93
Ryeker <ryekerjh@gmail.com>
2018-01-18 20:59:42 -0800

    learned about primitives and objects and variables.

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+// var yes = false;
+
+// console.log(yes);
+
+{ //Block A
+  { //Block B
+    {//Block C
+      {
+        {
+          
+        }
+      }
+    }
+  }
+}
+
+
+{
+  // let firstNumber;
+  // firstNumber = 22;
+  let firstNumber = 22; //Declaration and definition
+  // console.log(firstNumber);
+}
+
+var secondNumber = 11;
+
+secondNumber = 1111;
+
+
+// console.log(secondNumber);
+
+// console.log('testing testing');
+
+
+
+
+
+
+
+// 
+// 
+// firstNumber = "hey there, 344";
+// console.log(firstNumber);
+{
+  let truth = "Ep8 was....TERRIBLE...";
+  // console.log(truth);
+}
+
+var truth = "ep8 was...meh...";
+// console.log(truth);
+
+let rightNow = new Date();
+// console.log(Date.parse(rightNow));
+
+var iLikePi = Math.PI;
+// console.log(iLikePi);
+
+var number = 6;
+
+var objNumber = new Number("");
+
+console.log(number);
+console.log(objNumber);
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+//
