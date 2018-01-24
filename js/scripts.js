0a65966b6f1f342aa252d1822d8bd3384f4bd9a6
Ryeker <ryekerjh@gmail.com>
2018-01-23 18:59:23 -0800

    day 2 stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,124 @@
-// var yes = false;
-
-// console.log(yes);
-
-{ //Block A
-  { //Block B
-    {//Block C
-      {
-        {
-          
-        }
-      }
-    }
-  }
-}
-
-
-{
-  // let firstNumber;
-  // firstNumber = 22;
-  let firstNumber = 22; //Declaration and definition
-  // console.log(firstNumber);
-}
-
-var secondNumber = 11;
-
-secondNumber = 1111;
-
-
-// console.log(secondNumber);
-
-// console.log('testing testing');
-
-
-
-
-
-
-
+// // var yes = false;
+// 
+// // console.log(yes);
+// 
+// { //Block A
+//   { //Block B
+//     {//Block C
+//       {
+//         {
+// 
+//         }
+//       }
+//     }
+//   }
+// }
+// {
+//   // let firstNumber;
+//   // firstNumber = 22;
+//   let firstNumber = 22; //Declaration and definition
+//   // console.log(firstNumber);
+// }
+// 
+// var secondNumber = 11;
+// 
+// secondNumber = 1111;
 // 
 // 
-// firstNumber = "hey there, 344";
-// console.log(firstNumber);
-{
-  let truth = "Ep8 was....TERRIBLE...";
-  // console.log(truth);
-}
-
-var truth = "ep8 was...meh...";
-// console.log(truth);
-
-let rightNow = new Date();
-// console.log(Date.parse(rightNow));
-
-var iLikePi = Math.PI;
-// console.log(iLikePi);
-
-var number = 6;
-
-var objNumber = new Number("");
-
-console.log(number);
-console.log(objNumber);
-
-
-
-
-
-//
-
-
-
-
+// // console.log(secondNumber);
+// 
+// // console.log('testing testing');
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// // 
+// // 
+// // firstNumber = "hey there, 344";
+// // console.log(firstNumber);
+// {
+//   let truth = "Ep8 was....TERRIBLE...";
+//   // console.log(truth);
+// }
+// 
+// var truth = "ep8 was...meh...";
+// // console.log(truth);
+// 
+// let rightNow = new Date();
+// // console.log(Date.parse(rightNow));
+// 
+// var iLikePi = Math.PI;
+// // console.log(iLikePi);
+// 
+// var number = 6;
+// 
+// var objNumber = new Number("");
+// // 
+// // console.log(number);
+// // console.log(objNumber);
+// 
+// 
+// 
+// 
+// 
+// //
+// var myArray = ["Hello World", 22, true, {}, null];
+// // console.log(myArray.length);
+// 
+// 
+// 
+// 
+// 
+// var myObject = {
+//   myString: "Hello World",
+//   magicNumber: 42
+// };
+// 
+// // let alertMessage = alert("Hey, there! My book is only $9.99....");
+// // let confirmMessage = confirm("Is your name Jesus?");
+// let promptMessage = prompt("Tell me the password...", "password");
+// console.log(promptMessage);
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// //
+const firstName = prompt("What is your first name?", "Maria");
+const lastName = prompt("WHat is your last name?", "is the best...duh");
+console.log(firstName);
+alert(lastName);
+let birthday = prompt("What's your birthday?", "Please answer in the following format: 1/1/1900");
+confirm(new Date(birthday));
 
 
 
