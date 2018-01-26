b3ef3a24ee0d934936f6f611a0e936c82b87311e
Ryeker <ryekerjh@gmail.com>
2018-01-25 19:52:14 -0800

    adding day 3

--- js/scripts.js
+++ js/scripts.js
@@ -113,15 +113,113 @@
 // 
 // 
 // //
-const firstName = prompt("What is your first name?", "Maria");
-const lastName = prompt("WHat is your last name?", "is the best...duh");
-console.log(firstName);
-alert(lastName);
-let birthday = prompt("What's your birthday?", "Please answer in the following format: 1/1/1900");
-confirm(new Date(birthday));
+// const firstName = prompt("What is your first name?", "Maria");
+// const lastName = prompt("WHat is your last name?", "is the best...duh");
+// console.log(firstName);
+// alert(lastName);
+// let birthday = prompt("What's your birthday?", "Please answer in the following format: 1/1/1900");
+// confirm(new Date(birthday));
 
 
 
 //This is my gh-pages branch 
+
+//
+
+var string = true;
+var number = 1;
+// console.log(string != number);
+
+// let obj1 = {name:"Ryeker"};
+// let obj2 = {name:"Ryeker"};
+
+
+
+// console.log(obj1 === obj2);
+// console.log(!firstName || !lastName);
+
+// var primitive = "Hello World!";
+// var stringObject = new String("Hello World!");
+// console.log(primitive === stringObject);
+// console.log(primitive.length);
+// console.log(typeof(primitive));
+var number1 = "12";
+var number2 = "780";
+number1 = new Number(number1);
+number2 = new Number(number2);
+parseInt();
+// console.log(number1 + number2);
+number1 += 12;
+// console.log(number1);
+
+// number1--;
+// console.log(isNaN(number1));
+// console.log(number1);
+// console.log(typeof(stringObject));
+
+
+
+
+
+const bestMovie = [];
+// bestMovie != undefined ? alert('it exists!') : alert('It doesn\'t exist!');
+
+// if(typeof(bestMovie) === 'string') {
+//   alert('Yay its a string!');
+// } else if(typeof(bestMovie) === 'number') {
+//   alert('You gave me a number');
+// } else {
+//   alert('It\'s not a string or a number!');
+// }
+
+// switch(typeof(bestMovie)) {
+//   case 'string':
+//     alert('Yay it\'s a string!');
+//     break;
+//   case 'number':
+//     alert('A number!');
+//     break;
+//   case 'boolean':
+//     alert('A bool!');
+//     break;
+//   case 'object':
+//     alert('An object!');
+//     break;
+//   default:
+//     alert('its an array');
+//     break;
+// }
+
+var userAge = confirm('Are you over 18?');
+
+if(userAge == true) {
+  underEighty = confirm("Are you under 80 years old?");
+  if(underEighty) {
+    var starWars = confirm("Do you like star wars?");
+    if(starWars) {
+      alert("You rock");
+    }
+  } else {
+    confirm("Do you like prunes?");
+  }
+} else {
+  alert('Youre not old enough...thats great kid, dont get cocky');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
