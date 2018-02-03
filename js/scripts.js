0ef9479278ee71db36535840e7f395b93015887a
Ryeker <ryekerjh@gmail.com>
2018-02-02 20:22:49 -0800

    adding work for functions

--- js/scripts.js
+++ js/scripts.js
@@ -9,67 +9,81 @@
 // // var userLogin = function(username, password) {
 // //   //compare correct password for this user with the password they give you
 // // };
-// var argument1 = "cheese";
+// var a = "cheese";
+// 
+// myMagicFunction(a, "STar Trek");
+// 
 // //Named function that takes 2 arguments
 // function myMagicFunction(argument1, argument2) {
 //   console.log(`${argument1} is better than ${argument2}`);
 // };
 // 
-// myMagicFunction(argument1, "STar Trek");
+// function callMyself(number) {
+//   if(number >= 0) {
+//     number--;
+//     console.log(number);
+//   } else {
+//     return
+//   }
+// }
+// 
+// (number) => {
+//   if(number >= 0) {
+//     number--;
+//     console.log(number);
+//   } else {
+//     return
+//   }
+// }
+// function givesMeBestNumber(number) {
+//   if(number === 42) {
+//     return `${number} is the best number!`;
+//   } else {
+//     alert(`${number} sucks!!!`);
+//   }
+// }
+// 
+// 
+// 
+// 
+// let message = givesMeBestNumber(42);
+// 
+// // alert(message);
+// 
+// 
+// 
+// 
 
+// const dogAge = prompt("How old is dat dog doe?", '3');
 
-
-//TAKEHOME
-
-//STEP 1
-var todos = [];
-var todoOne = prompt("what do you want to do this weekend?");
-var todoTwo = prompt("What else do you want to do this weekend?");
-var todoThree = prompt("What is a third thing you want to do this weekend?");
-
-todos.push(todoOne, todoTwo, todoThree);
-
-//STEP 2
-var todosWithDays = [];
-for(let i = 0; i < todos.length; i++) {
-  var due = prompt(`What day do you want to get ${todos[i]} done?`, 'Please use this format: day/month/year');
-  due = new Date(due);
-  todosWithDays.push([todos[i], due]);
-}
-
-//STEP 3
-var sortedWithIndices = [];
-let j = 0;
-var now = new Date().getTime();//Converts JS Date object to ms
-while (j < todosWithDays.length) {
-  let oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  var diffDays = Math.round((todosWithDays[j][1].getTime() - now)/oneDay);
-  sortedWithIndices.push([diffDays, todosWithDays[j]]);
-  j++
-}
-
-sortedWithIndices.sort();
-
-console.log(sortedWithIndices[sortedWithIndices.length - 1][1], ' will take the longest');
-
-//STEP 4
-let n = 0;
-
-do {
-  if(n != sortedWithIndices.length - 1) {
-    sortedWithIndices[n][1].push("done");
-  }
-  n++
-} while(n < sortedWithIndices.length);
-
-
-let alertArr = [];
-for(let i = 0; i < sortedWithIndices.length; i++) {
-  if(sortedWithIndices[i][1].indexOf('done')s) {
-    alertArr.push(sortedWithIndices[i][1]);
+function calcDogYears(age) {
+  parseInt(age);
+  if(isNaN(age)) {
+    //If the user gives me a non-number...
+    calcDogYears(prompt('Nah, for real...NUMBER', '10'));
+  } else {
+    alert(age *= 7);//this is this age = age * 7
   }
 }
 
+// calcDogYears();
 
-//STep 5
-alert(alertArr);
+
+var myString = "lalala";
+
+myString.toUpperCase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
