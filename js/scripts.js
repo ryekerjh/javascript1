00f37d00b685e7126404b4d099a7cf6604cc3bfa
Ryeker <ryekerjh@gmail.com>
2018-02-08 16:56:35 -0800

    day 7

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var pTag = document.getElementById("my-ptag");
+
+pTag.style.color = "purple";
+
+var h1 = document.querySelector('h1');
+
+h1.addEventListener("mouseover", function() {
+  //DO STUFF
+  h1.style.color = "blue";
+});
