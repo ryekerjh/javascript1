2ac2c2342036e72f16a27a7903d6b47765649346
Ryeker <ryekerjh@gmail.com>
2018-02-08 17:30:16 -0800

    day 6 in class

--- js/scripts.js
+++ js/scripts.js
@@ -26,21 +26,45 @@ myKey.addEventListener('keydown', function(e) {
 
 //1.  On your day6 branch, create a basic HTML mockup with a button, a div with some height and background color, and an h1 with some text
 //Create event listeners for each:
-// For the button, alert the user with a message when they click it.
-// For the div, change the color of the background when their mouse goes into the div.
-// For the h1, change the color of the text the user releases any key.
-// Not Tough Enough? Try redirecting the user's browser to facebook when the click anywhere on the page.
 
 var myDiv = document.querySelector('.my-div'),
-mybutton = document.querySelector('button'),
+myButton = document.querySelector('button'),
 myTitle = document.querySelector('.my-title');
+
+// For the button, alert the user with a message when they click it.
+myButton.addEventListener("click", () => {
+  alert("How darest thou?!");
+});
+
+// For the div, change the color of the background when their mouse goes into the div.
+color = ["blue", "green", "black", "purple", "yellow"];
 
 myDiv.addEventListener('mouseenter', function(e) {
   //TODO: Write array for diff colors and select randomly
-  e.target.style.backgroundColor = "green";
+  e.target.style.backgroundColor = randomColor(0, color.length - 1);
+});
+
+function randomColor(min, max) {
+  var num = Math.round(Math.random() * (max - min) + min);
+  console.log(color[num], num);
+  return color[num];
+};
+
+// For the h1, change the color of the text the user releases any key.
+var myInput = document.querySelector(".keydownExample");
+
+myInput.addEventListener("keyup", (e) => {
+  myTitle.style.color = randomColor(0, color.length - 1);
 })
 
+// Not Tough Enough? Try redirecting the user's browser to facebook when the click anywhere on the page.
 
+var body = document.querySelector("body");
+
+body.addEventListener("click", (e) => {
+  window.location = "https://www.facebook.com";
+  // console.log(e.target);
+})
 
 
 
